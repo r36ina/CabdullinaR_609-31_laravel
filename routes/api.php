60506d0bd6c999ba25e598943ed9c6b71beab63f
regina <rgabdullina@ya.ru>
2026-02-24 00:10:11 +0500

    installation Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\PetControllerApi;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceControllerApi;
 use App\Http\Controllers\MedWorkerControllerApi;
+use Illuminate\Http\Request;
 
-Route::get('/services', [ServiceControllerApi::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/services/{id}', [ServiceControllerApi::class, 'show']);
 
 Route::get('/medworkers', [MedWorkerControllerApi::class, 'index']);
@@ -13,3 +16,11 @@ Route::get('/medworkers/{id}', [MedWorkerControllerApi::class, 'show']);
 
 Route::get('/pets', [PetControllerApi::class, 'index']);
 Route::get('/pets/{id}', [PetControllerApi::class, 'show']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/services', [ServiceControllerApi::class, 'index']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+   Route::get('/logout', [AuthController::class, 'logout']);
+});
