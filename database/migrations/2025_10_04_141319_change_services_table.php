abe7b9f9505b944eeb07b78df53c25a52a8b9d7d
regina <rgabdullina@ya.ru>
2026-09-17 23:10:33 +0500

    fix

--- database/migrations/2025_10_04_141319_change_services_table.php
+++ database/migrations/2025_10_04_141319_change_services_table.php
@@ -1,27 +1,23 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::table('services', function (Blueprint $table) {
-            $table->dropPrimary('name');
-            $table->id()->primary();
-        });
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::statement('ALTER TABLE `services` DROP PRIMARY KEY');
+        DB::statement('ALTER TABLE `services` ADD `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`)');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::statement('ALTER TABLE `services` DROP COLUMN `id`');
+        DB::statement('ALTER TABLE `services` ADD PRIMARY KEY (`id`)'); // Замените на старое имя ключа, если помните его
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 };
