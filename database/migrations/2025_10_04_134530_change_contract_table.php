ca8cae68d63395cd75f09a1e31ce2665030739f7
regina <rgabdullina@ya.ru>
2026-09-17 22:44:14 +0500

    fix

--- database/migrations/2025_10_04_134530_change_contract_table.php
+++ database/migrations/2025_10_04_134530_change_contract_table.php
@@ -1,37 +1,23 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Schema::table('contract', function (Blueprint $table) {
-            $table->dropPrimary('contract_number');
-            $table->id()->primary();
-        });
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::statement('ALTER TABLE `contract` DROP PRIMARY KEY');
+        DB::statement('ALTER TABLE `contract` ADD `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`)');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
-        Schema::table('contract', function (Blueprint $table) {
-            $table->dropPrimary('id');
-            $table->primary('contract_number');
-        });
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::statement('ALTER TABLE `contract` DROP COLUMN `id`');
+        DB::statement('ALTER TABLE `contract` ADD PRIMARY KEY (`contract_number`)');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 };
