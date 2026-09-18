a05464e9bb825fcb5aa982434737a5d08b493b30
regina <rgabdullina@ya.ru>
2026-09-18 16:58:37 +0500

    fix

--- database/migrations/2025_09_22_110420_create_medworker_table.php
+++ database/migrations/2025_09_22_110420_create_medworker_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('job_title');
             $table->string('email')->unique();
             $table->string('phone')->unique();
+            $table->string('specialization')->nullable();
             $table->timestamps();
 
         });
